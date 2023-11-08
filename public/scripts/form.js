315e5d290cf73bf33edde2ce95ce096da476802d
Dens26 <denis.bekaert@live.fr>
2023-11-08 17:55:37 +0100

    v3.0.3
    Gestion du formulaire avec FormData

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -3,12 +3,11 @@ import { createId, SaveTaskTab, LoadTaskTab } from "../modules/functions.js";
 import { TaskObject } from "../modules/class.js";
 
 // Référence DOM
-const submitButton = document.querySelectorAll(".submit-button");
-const inputName = document.getElementById("name");
-const inputDescription = document.getElementById("description");
+const formTask = document.querySelector("#form-task");
 
 // Récupération du tableau de tâche et de la tâche à modifier
 const edit = JSON.parse(sessionStorage.getItem("Edit"));
+
 // Récupération du tableau
 let taskTab = await LoadTaskTab("form.html");
 if (taskTab.toString().substr(0, 10) == 'Error: 500')
@@ -18,22 +17,26 @@ if (taskTab.toString().substr(0, 10) == 'Error: 500')
 let params = new URLSearchParams(document.location.search);
 if (params.get('value') == "edit") {
     // Affichage des valeurs dans le formulaire
-    inputName.value = edit.name;
-    inputDescription.value = edit.description;
-    submitButton[0].textContent = "Modifier";
+    formTask[0].value = edit.name;
+    formTask[1].textContent = edit.description;
+    formTask[2].textContent = "Modifier";
 }
 
 // Ecouteur sur le bouton "Ajouter"
-submitButton[0].addEventListener("click", async (evt) => {
+formTask.addEventListener("submit", async (evt) => {
     // Si la tâche n'est pas null
-    if (inputName.value.length != 0 && inputDescription.value.length != 0) {
+    const formData = new FormData(formTask);
+    const name = formData.get("name");
+    const description = formData.get("description");
+
+    if (name.length != 0 && description.length != 0) {
         evt.preventDefault();
         if (params.get('value') == "new") {
             // Création de l'id
             let id = createId(taskTab);
 
             // Nouvelle variables task
-            taskTab.push(new TaskObject(id, inputName.value, inputDescription.value, false));
+            taskTab.push(new TaskObject(id, name, description, false));
             await SaveTaskTab(taskTab);
         }
         else if (params.get('value') == "edit") {
@@ -41,8 +44,8 @@ submitButton[0].addEventListener("click", async (evt) => {
             let task = taskTab.find(data => data.id == edit.id);
 
             // Modification de la tâche
-            task.name = inputName.value;
-            task.description = inputDescription.value;
+            task.name = name;
+            task.description = description;
 
             // Enregistrement
             await SaveTaskTab(taskTab);
